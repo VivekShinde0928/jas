d857b00cff12437845d63df0908d952f7db50757
vivek <vivek@mobitrail.com>
2025-03-19 11:17:06 +0530

    moved wsgi to orginal folder

--- jas/jas/wsgi.py
+++ jas/jas/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'jas.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'jas.jas.settings')
 
 application = get_wsgi_application()
